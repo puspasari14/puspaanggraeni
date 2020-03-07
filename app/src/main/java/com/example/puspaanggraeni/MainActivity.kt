3c317195fdfa34e9890f6f8336611c856f56dd55
puspasari14 <puspasaridiananggraeni14@gmail.com>
2020-03-07 21:44:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/puspaanggraeni/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.puspaanggraeni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_me)
+    }
+}
